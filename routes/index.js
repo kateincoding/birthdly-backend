6f0497cf534a62019947021580fe27aadf4a3276
kate <kateincoding@gmail.com>
2022-06-12 03:39:35 -0500

    [routing of birthday]

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const usersRouter = require('./usersRouter');
-// const birthRouter = require('./birthRouter');
-// const categoryRouter = require('./categoryRouter');
+const birthRouter = require('./birthRouter');
+const categoryRouter = require('./categoryRouter');
 
 function routerApi(app) {
     const router = express.Router();
     app.use('/api/v1', router);
     router.use('/users', usersRouter);
-//    router.use('/births', birthRouter);
-//    router.use('/categories', categoryRouter);
+    router.use('/births', birthRouter);
+    router.use('/categories', categoryRouter);
 }
 
 module.exports = routerApi;
